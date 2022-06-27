0e7cae3d56bdbac7c4186123d30ca34b0722f027
willp11 <williampage11@yahoo.co.uk>
2022-06-27 10:15:34 +0700

    changed custom user back to just name

--- backend/accounts/forms.py
+++ backend/accounts/forms.py
@@ -4,7 +4,7 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class meta(UserCreationForm):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('first_name', 'last_name',)
+        fields = UserCreationForm.Meta.fields + ('name',)
 
 class CustomUserChangeForm(UserChangeForm):
     class meta(UserChangeForm):
--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    first_name = models.CharField(max_length=100, null=True, blank=True)
-    last_name = models.CharField(max_length=100, null=True, blank=True)
+    name = models.CharField(max_length=100, null=True, blank=True)
     email_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
