1f320666c8480d04454cbd7e76e3a70a0f25e940
willp11 <williampage11@yahoo.co.uk>
2022-07-07 10:16:08 +0700

    added search functions to chat contacts component

--- frontend/src/components/ChatContacts/ChatContacts.js
+++ frontend/src/components/ChatContacts/ChatContacts.js
@@ -2,12 +2,55 @@ import './ChatContacts.css';
 import {useState, useEffect, useCallback} from 'react';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const ChatContacts = () => {
 
     const token = useSelector((state)=>state.auth.token);
 
+    // list of all classes with student lists nested inside that contain parent info
     const [contactList, setContactList] = useState(null);
+
+    // student and class names to search for
+    const [classNameSearch, setClassNameSearch] = useState("");
+    const [studentNameSearch, setStudentNameSearch] = useState("");
+
+    // show search results
+    const [studentFound, setStudentFound] = useState(null);
+    const [showSearchResults, setShowSearchResults] = useState(false);
+
+    // FUNCTION SEARCH CONTACTS
+    // find student, given class and student's name
+    const searchStudents = (className, studentName) => {
+        let student = null;
+        // trim whitespace
+        className = className.trim()
+        studentName = studentName.trim()
+
+        contactList.school_classes.forEach((schoolClass)=>{
+            if (schoolClass.name === className) {
+                // found className
+                schoolClass.students.forEach((stud) => {
+                    // found student
+                    if (stud.name === studentName) {
+                        student = stud;
+                    }
+                })
+            }
+        })
+
+        setStudentFound(student);
+        setShowSearchResults(true);
+        return student;
+    }
+    // search go back button
+    const searchGoBack = () => {
+        setShowSearchResults(false);
+        setStudentFound(null);
+        setClassNameSearch("");
+        setStudentNameSearch("");
+    }
+
 
     // FUNCTION GET ALL USER'S CONTACTS
     const getTeacherContacts = useCallback(() => {
@@ -31,6 +74,48 @@ const ChatContacts = () => {
         getTeacherContacts();
     }, [getTeacherContacts])
 
+    // SEARCH DIVS
+    // search inputs
+    let search_students_div = null;
+    if (contactList) {
+        search_students_div = (
+            <div>
+                <input placeholder="Class Name" onChange={(e)=>setClassNameSearch(e.target.value)} />
+                <input placeholder="Student Name" onChange={(e)=>setStudentNameSearch(e.target.value)} />
+                <button onClick={()=>searchStudents(classNameSearch, studentNameSearch)}>Submit</button>
+            </div>
+        )
+    }
+    // search results
+    let search_results_div = null;
+    if (studentFound) {
+        let parent = <p>This student has no parent account.</p>;
+        if (studentFound.parent !== null) {
+            parent = <p><b>Parent:</b> {studentFound.parent.user.first_name} {studentFound.parent.user.last_name}</p>
+        }
+    
+        search_results_div = (
+            <div>
+                <h3>Search Results</h3>
+                <button onClick={searchGoBack}>Go Back</button> <br/>
+                <div style={{border: "1px solid grey", marginTop: "10px"}} >
+                    <p><b>Student: </b><Link to={`/studentProfile/${studentFound.id}`}>{studentFound.name}</Link></p>
+                    {parent}
+                </div>
+            </div>
+        )
+    } else {
+        search_results_div = (
+            <div>
+                <h3>Search Results</h3>
+                <button onClick={searchGoBack}>Go Back</button> <br/>
+                <div style={{border: "1px solid grey", marginTop: "10px"}} >
+                    <p>No student found.</p>
+                </div>
+            </div>
+        )
+    }
+
     // SCHOOL CLASSES DIV
     let school_classes_div = null;
     if (contactList) {
@@ -42,23 +127,32 @@ const ChatContacts = () => {
                 }
                 return (
                     <div style={{border: "1px solid grey"}} key={student.id} >
-                        <p><b>Student:</b> {student.name}</p>
+                        <p><b>Student: </b><Link to={`/studentProfile/${student.id}`}>{student.name}</Link></p>
                         {parent}
                     </div>
                 )
             })
             return (
                 <div key={school_class.id}>
+                    <h3>{school_class.name}</h3>
                     {students}
                 </div>
             )
         })
     }
 
+    // Search inputs and contact list in same div - show when no student is found
+    let search_and_contacts_div = (
+        <div>
+            {search_students_div}
+            {school_classes_div}
+        </div>
+    )
+
     return (
         <div className="ChatContacts">
             <h2>Contacts</h2>
-            {school_classes_div}
+            {showSearchResults ? search_results_div : search_and_contacts_div}
         </div>
     )
 }
