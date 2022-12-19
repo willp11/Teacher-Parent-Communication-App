9a7551b3e7f8ebfcc1a53e374f647962e3fda1c1
willp11 <williampage11@yahoo.co.uk>
2022-12-19 23:58:30 +0700

    fixed notifications runtime error

--- frontend/src/components/Navigation/Navigation.js
+++ frontend/src/components/Navigation/Navigation.js
@@ -32,9 +32,11 @@ const Navigation = () => {
 
     const qtyNotifications = useMemo(()=>{
         let count = 0;
-        notifications.forEach((notification)=>{
-            if (!notification.read) count += 1
-        })
+        if (notifications) {
+            notifications.forEach((notification)=>{
+                if (!notification.read) count += 1
+            })
+        }
         if (count > 0) return count
         return null;
     }, [notifications])
