fe1276a65a9ba953e583ab79f7ec93c3b0d1c5ee
willp11 <williampage11@yahoo.co.uk>
2022-12-22 19:34:45 +0700

    deleted db from production docker settings as use managed database instead

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -130,7 +130,7 @@ DATABASES = {
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
         'HOST': os.environ.get('DB_HOST'),
-        # 'PORT': os.environ.get('DB_PORT'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
